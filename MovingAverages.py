bf28c49e19729e4bf35eae2690a103015d3066c6
naslevente <levente.nas@gmail.com>
2020-05-27 19:38:47 -0400

    Add Exponential Mocing Average

--- MovingAverages.py
+++ MovingAverages.py
@@ -10,15 +10,16 @@ class MovingAverages():
     global tickerData
     tickerSymbol = yf.Ticker('AAPL')#Identifies which stock to look at --> in this case Apple
     tickerData = tickerSymbol.history(period = '1y', interval = '1d', start = '2020-3-1', end = '2020-5-24')#Paramters for data of stock(constructs data frame)
-    
-        
+
+
 
     def SimpleMovingAverage():
         timeframe = int(input("How many days is your simple moving average?: "))
         print(tickerData['Close'].rolling(window = timeframe).mean())
 
 
+    def ExponentialMovingAverage():
+
+        
 
     SimpleMovingAverage() #Test for SMA Method
-
-
