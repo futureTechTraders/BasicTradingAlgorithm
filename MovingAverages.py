31bb042c9d1112252169fa8d96fd63b8abbf4f09
akhil <akhilmaheeps12@gmail.com>
2020-05-27 14:40:51 -0400

    Class for all moving average methods, created Simple moving average

---
+++ MovingAverages.py
@@ -0,0 +1,24 @@
+import yfinance as yf
+import pandas as pd
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Create Dataframe -> Later optimize for user input
+class MovingAverages():
+    global tickerSymbol
+    global tickerData
+    tickerSymbol = yf.Ticker('AAPL')#Identifies which stock to look at --> in this case Apple
+    tickerData = tickerSymbol.history(period = '1y', interval = '1d', start = '2020-3-1', end = '2020-5-24')#Paramters for data of stock(constructs data frame)
+    
+        
+
+    def SimpleMovingAverage():
+        timeframe = int(input("How many days is your simple moving average?: "))
+        print(tickerData['Close'].rolling(window = timeframe).mean())
+
+
+
+    SimpleMovingAverage() #Test for SMA Method
+
+
