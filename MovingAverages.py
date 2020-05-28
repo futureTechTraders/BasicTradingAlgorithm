148a7634da778a6d2854eb75af13a1d6492005a4
naslevente <levente.nas@gmail.com>
2020-05-27 23:21:19 -0400

    Update MovingAverages EMA and SMA

--- MovingAverages.py
+++ MovingAverages.py
@@ -8,19 +8,21 @@ import numpy as np
 class MovingAverages():
     global tickerSymbol
     global tickerData
+    global timeframe
     tickerSymbol = yf.Ticker('AAPL')#Identifies which stock to look at --> in this case Apple
     tickerData = tickerSymbol.history(period = '1y', interval = '1d', start = '2020-3-1', end = '2020-5-24')#Paramters for data of stock(constructs data frame)
 
-    def ExponentialMovingAverage(timeFrame):
+    timeframe = int(input("How many days is your moving average: "))
 
-        ema = tickerData['Close'].ewm(span = timeFrame, adjust = False).mean()
+    def ExponentialMovingAverage(timeframe):
+        ema = tickerData['Close'].ewm(span = timeframe, adjust = False).mean()
+        ema.plot(grid = True)
         print(ema)
 
-    def SimpleMovingAverage():
-        timeframe = int(input("How many days is your simple moving average?: "))
-        tickerData['Close'].plot(grid = True)
+    def SimpleMovingAverage(timeframe):
+        tickerData['Close'].plot()
         print(tickerData['Close'].rolling(window = timeframe).mean())
-        ExponentialMovingAverage(timeframe)
+        plt.show()
 
-
-    SimpleMovingAverage() #Test for SMA Method
+    ExponentialMovingAverage(timeframe) #Test for EMA Method
+    SimpleMovingAverage(timeframe) #Test for SMA Method
