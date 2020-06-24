b294566dc240fc4f50b900f7d72d005b307d9fbc
akhil <akhilmaheeps12@gmail.com>
2020-06-24 10:04:07 -0400

    Added yfinance parameter documentation as comments at the bottom of file

--- TickerDataFrame.py
+++ TickerDataFrame.py
@@ -3,12 +3,29 @@ import pandas as pd
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
-
+from datetime import date
 class TickerDataFrame:
     def __init__(self,tSymbol):
         self.tickerSymbol = tSymbol
         symbol = yf.Ticker(self.tickerSymbol)
-        tickerData = symbol.history(period = '1y', interval = '1d', start = '2020-3-1', end = '2020-5-15')#Edit these parameters for user input later
+    def createDataFrame(self):
+        symbol = yf.Ticker(self.tickerSymbol)
+        tickerData = symbol.history(period = '1d', start = date.today())
+        #tickerData = symbol.history(period = '1y', interval = '1d', start = '2020-3-1', end = '2020-5-28')#Edit these parameters for user input later
+        return tickerData
+
+
+stockTicker = input("what stock would you like data for?: " )
+df = TickerDataFrame(stockTicker)
+pd.set_option('display.max_rows', None)
+print(df.createDataFrame())
+
+'''
+YFinance Parameters:
+Period: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max DEFAULT IS 1mo
+Interval: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo DEFAULT IS 1d
+
+
 
 
 
